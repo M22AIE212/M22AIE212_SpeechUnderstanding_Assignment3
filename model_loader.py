aaefa81eeb5f8a43b7b0622747c604d3f843cc0a
M22AIE212 <142037702+M22AIE212@users.noreply.github.com>
2024-04-30 19:55:21 +0530

    Create model_loader.py

---
+++ model_loader.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class ModelLoader:
+    def __init__(self, args, device):
+        self.args = args
+        self.device = device
+
+    def load_model(self, task):
+        model = Model(self.args, self.device)
+        total_parameters = sum([param.view(-1).size()[0] for param in model.parameters()])
+
+        if task.lower() == 'la':
+            model = model.to(self.device)
+            print('Total parameters:', total_parameters)
+            model.load_state_dict(torch.load(self.args.la_model_path, map_location=self.device))
+            print('Model loaded:', self.args.la_model_path)
+        else:
+            model = nn.DataParallel(model).to(self.device)
+            print('Total parameters:', total_parameters)
+            model.load_state_dict(torch.load(self.args.df_model_path, map_location=self.device))
+            print('Model loaded:', self.args.df_model_path)
+
+        return model
